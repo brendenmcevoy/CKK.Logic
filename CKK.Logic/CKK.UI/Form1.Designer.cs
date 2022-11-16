41d778e128711172e20cbb8473b7bff1bc3557b7
Brenden McEvoy <brenden.mcevoy24@gmail.com>
2022-11-15 18:52:40 -0700

    begin UI

---
+++ CKK.Logic/CKK.UI/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace CKK.UI
+{
+    partial class InventoryManager
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.AutoSize = true;
+            this.addButton.BackColor = System.Drawing.Color.Black;
+            this.addButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.addButton.ForeColor = System.Drawing.Color.White;
+            this.addButton.Location = new System.Drawing.Point(12, 12);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(166, 50);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add Item(s)";
+            this.addButton.UseVisualStyleBackColor = false;
+            // 
+            // removeButton
+            // 
+            this.removeButton.AutoSize = true;
+            this.removeButton.BackColor = System.Drawing.Color.Black;
+            this.removeButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.removeButton.ForeColor = System.Drawing.Color.White;
+            this.removeButton.Location = new System.Drawing.Point(200, 12);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(166, 50);
+            this.removeButton.TabIndex = 1;
+            this.removeButton.Text = "Remove Item(s)";
+            this.removeButton.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoSize = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(12, 77);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1289, 727);
+            this.panel1.TabIndex = 2;
+            // 
+            // InventoryManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1313, 816);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Name = "InventoryManager";
+            this.Text = "Inventory Manager";
+            this.Load += new System.EventHandler(this.InventoryManager_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
